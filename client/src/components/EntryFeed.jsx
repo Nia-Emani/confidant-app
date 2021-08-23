7eafe3cd33fd3f158cd91689cbaad5d7566992ed
niaemani <niaemanidickson@gmail.com>
2021-08-23 16:09:57 -0400

    axios get, post, and edit request foundation

--- client/src/components/EntryFeed.jsx
+++ client/src/components/EntryFeed.jsx
@@ -1,6 +1,22 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { baseURL, config } from '../services'
+
 function EntryFeed(props) {
+  const [entries, setEntries] = useState([])
+  const [toggleFetch, setToggleFetch] = useState(false)
+
+  useEffect(() => {
+    const getEntries = async () => {
+      const resp = await axios.get(baseURL, config)
+      setEntries(resp.data.records)
+      console.log(resp.data)
+    }
+    getEntries()
+  }, [toggleFetch])
+
   return (
-    <h1>This is my Journal Feed</h1>
+    <h1>This is my Journal Feed!</h1>
   )
 }
 
--- client/src/components/NewEntry.jsx
+++ client/src/components/NewEntry.jsx
@@ -1,15 +1,30 @@
 import { useParams } from "react-router-dom"
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { baseURL, config } from '../services'
 
 function NewEntry(props) {
-  const [newThought, setNewThought] = useState('')
+  const [thought, setThought] = useState('')
+  const [date, setDate] = useState('')
 
   const params = useParams()
 
+  // useEffect(() => {
+  //   if (params.id && props.thoughts.length > 0) {
+  //     const thoughtToEdit = props.thoughts.find(thought => params.id === thought.id)
+  //     if (thoughtToEdit) {
+  //       setDate(thoughtToEdit.fields.date)
+  //       setThought(thoughtToEdit.fields.thought)
+  //     }
+  //   }
+  // }, [params.id, props.thoughts])
+
   const handleSubmit = async (event) => {
     event.preventDefault()
-    const newThought = { entry }
+    const newThought = {
+      thought,
+      date
+    }
     if (params.id) {
       await axios.put(`${baseURL}/${params.id}`, { fields: newThought }, config)
     } else {
@@ -22,11 +37,19 @@ function NewEntry(props) {
     <div className="new-entry">
       <h1>Hi there!<br />What's on your mind?</h1>
       <form onSubmit={handleSubmit}>
+        <label htmlFor='date'>Date</label>
         <input
-          id='entry'
+          id='date'
           type='text'
-          onChange={e => setNewThought(e.target.value)}
-          value={entry}
+          onChange={e => setDate(e.target.value)}
+          value={date}
+        />
+        <label htmlFor='thought'>Jot down your thoughts here</label>
+        <input
+          id='thought'
+          type='text'
+          onChange={e => setThought(e.target.value)}
+          value={thought}
         />
         <button type="submit" className="new-entry-button">add entry</button>
       </form>
